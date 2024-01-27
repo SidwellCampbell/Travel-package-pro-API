5e283740eeda7d409925b686469031e54dc56697
Tevin Campbell <tevinc1991@icloud.com>
2024-01-27 13:20:01 -0500

    Add CheckoutController with /purchase endpoint
    A new CheckoutController class has been added in the TravelBug project. This controller is responsible for handling purchase requests with a POST method to the /purchase endpoint.

---
+++ src/main/java/com/travelbug/demo/controllers/CheckoutController.java
@@ -0,0 +1,30 @@
+package com.travelbug.demo.controllers;
+
+import com.travelbug.demo.services.CheckoutService;
+import com.travelbug.demo.services.Purchase;
+import com.travelbug.demo.services.PurchaseResponse;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+@PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.checkoutCart(purchase);
+
+        return purchaseResponse;
+
+
+}
+
+
+}
